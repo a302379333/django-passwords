df559f17a72b118ed107e8bc0040acf3b6bbf1c0
Alexandr <a302379333@gmail.com>
2014-04-04 17:16:43 +0700

    add compiled js file

--- passwords/static/passwords/passwords.js
+++ passwords/static/passwords/passwords.js
@@ -2,7 +2,7 @@
 (function() {
   jQuery(function($) {
     var bar_template, render_bar;
-    bar_template = '<div class="progress password <%=bar_type%>"><div class="bar" style="width: <%=width%>%;"></div></div><div class="help-block"><%=error_messages%></div>';
+    bar_template = '<div class="progress password <%=bar_type%>"><div class="bar" style="width: <%=width%>%;"></div></div><div class="password-validation-error"><%=error_messages%></div>';
     render_bar = _.template(bar_template);
     return $('input[name="password"]').keyup(function() {
       return $.post(window.password_validator_url, {
