7c01eca8722c716eae550d0932a536dd77206113
Alexandr <a302379333@gmail.com>
2014-04-14 12:01:01 +0700

    fix ui

--- passwords/static/passwords/passwords.coffee
+++ passwords/static/passwords/passwords.coffee
@@ -3,7 +3,8 @@ jQuery ($) ->
   bar_template = '<div class="progress password <%=bar_type%>"><div class="bar" style="width: <%=width%>%;"></div></div><div class="password-validation-error"><%=error_messages%></div>'
   render_bar = _.template(bar_template)
   $('input[name="password"]').keyup ->
+    mthis = @
     $.post(window.password_validator_url, {password: $(this).val()}, (response) ->
-      $(@).closest('p.help-block').remove()
+      $(mthis).closest('.controls').find('p.help-block').remove()
       $('.status_bar').html render_bar(response)
       )
--- passwords/static/passwords/passwords.js
+++ passwords/static/passwords/passwords.js
@@ -5,10 +5,12 @@
     bar_template = '<div class="progress password <%=bar_type%>"><div class="bar" style="width: <%=width%>%;"></div></div><div class="password-validation-error"><%=error_messages%></div>';
     render_bar = _.template(bar_template);
     return $('input[name="password"]').keyup(function() {
+      var mthis;
+      mthis = this;
       return $.post(window.password_validator_url, {
         password: $(this).val()
       }, function(response) {
-        $(this).closest('p.help-block').remove();
+        $(mthis).closest('.controls').find('p.help-block').remove();
         return $('.status_bar').html(render_bar(response));
       });
     });
