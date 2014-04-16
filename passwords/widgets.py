4ac554b0d046f930534ec54f2512f388fb22b90c
Alexandr <a302379333@gmail.com>
2014-04-16 17:09:12 +0700

    Add message

--- passwords/widgets.py
+++ passwords/widgets.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from django.forms import PasswordInput
 from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext as _
 
 
 class PasswordFieldWidget(PasswordInput):
@@ -19,9 +20,10 @@ class PasswordFieldWidget(PasswordInput):
             value = None
         if self.validate_url:
             data_render = """<div class="status_bar"><div class="progress password "><div class="bar" style="width: 0%;"></div></div></div>
+                            <p class="help-block">{message}</p>
             <script type="text/javascript">//global var for password validation url
                 window.password_validator_url = '{url}';
-            </script>""".format(url=self.validate_url)
+            </script>""".format(message=_(u'Пароль должен содержать от 6 до 20 символов, включая числа, заглавную и строчные буквы'), url=self.validate_url)
         else:
             data_render = '<div>Укажите урл для валидации поля</div>'
         return mark_safe(str(super(PasswordFieldWidget, self).render(name, value, attrs)) + data_render)
